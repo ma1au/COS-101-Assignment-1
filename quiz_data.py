9957f8c0f5008c75a90de10decff713b889f2cbd
victorrr <149414843+victorrr385@users.noreply.github.com>
2024-02-05 23:23:44 +0100

    i added the dictionary

--- quiz_data.py
+++ quiz_data.py
@@ -1,1 +1,27 @@
+quiz_data = [
+{ "question": "Who created python: ",
+ "choices": ["Guido Van Rossum", " Elon Musk", "Bill Gates", "Mark Zuberburg"],
+"answer": "Guido Van Rossum"
+},
+{"question":"What year was python created: ",
+      "choices": ["1989", "1999", "2000", "2016"],
+       "answer": "1999"
+},
+{"question":"Python was tributed to which comedy group: ",
+     "choices":["Lonely Island", "Smosh", "Monty Python", "SNL"],
+      "answer":"Monty Python"
+},
+{"question":"What influenced the design of Python? ",
+     "choices":["Fortran", "C++", "Assembly Language", "Pascal"],
+     "answer":"C++"
+},
+{"question":"What was the primary motivation behind creating Python?",
+     "choices":["High-performance computing", "Artificial Intelligence research", "Educational purposes", "Game development"],
+     "answer":"Educational purposes"
+}]
+
+
+
+
+
 
