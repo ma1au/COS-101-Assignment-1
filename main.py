4c65ed89bfef4bfd2b80356f49763e2fc543bc38
Alexander Ochuba <92830327+kennyochuba@users.noreply.github.com>
2024-02-06 00:20:09 +0100

    I added 3 functions

--- main.py
+++ main.py
@@ -32,3 +32,34 @@ def check_answer(choice):
         button.config(state="disabled")
     next_btn.config(state="normal")
 
+# Function to Restart the Quiz
+def restart_quiz():
+    global current_question, score
+    # Reset current_question and score
+    current_question = 0
+    score = 0
+    # Update the score label
+    score_label.config(text="Score: {}/{}".format(score, len(quiz_data)))
+
+    # Enable all choice buttons
+    for button in choice_btns:
+        button.config(state="normal")
+
+    # Display the first question
+    show_question()
+
+# Function to move on to the next question or end the quiz if the questions have finished
+def next_question():
+    global current_question
+    # Move to the next question
+    current_question += 1
+
+    # Check if there are more questions
+    if current_question < len(quiz_data):
+        # Display the next question
+        show_question()
+    else:
+        # Display a message box with the final score and exit the application
+        messagebox.showinfo("QUIZ COMPLETED",
+                            "QUIZ COMPLETED! Final Score: {}/{}".format(score, len(quiz_data)))
+        root.destroy()
