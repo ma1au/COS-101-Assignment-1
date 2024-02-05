7cacc478f0ad0b115d3e1c521cb1a4114b8e5052
MECKCOURAGE <148995056+MECKCOURAGE@users.noreply.github.com>
2024-02-05 23:12:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
+import tkinter as tk
+from tkinter import messagebox, ttk
+from ttkbootstrap import Style
+from quiz_data import quiz_data
+
+
+def show_question():
+    question = quiz_data[current_question]
+    qs_label.config(text=question["question"])
+
+    choices = question["choices"]
+    for i in range(4):
+        choice_btns[i].config(text=choices[i], state="normal")
+
+    feedback_label.config(text="")
+    next_btn.config(state="disabled")
+
+
+def check_answer(choice):
+    question = quiz_data[current_question]
+    selected_choice = choice_btns[choice].cget("text")
+
+    if selected_choice == question["answer"]:
+        global score
+        score += 1
+        score_label.config(text="Score: {}/{}".format(score, len(quiz_data)))
+        feedback_label.config(text="Correct!", foreground="green")
+    else:
+        feedback_label.config(text="Incorrect!", foreground="red")
+
+    for button in choice_btns:
+        button.config(state="disabled")
+    next_btn.config(state="normal")
 
